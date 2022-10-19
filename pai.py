77522d454f4cbcaff686d5af243056825c89ff3b
carlos alberto <carlosalbertosnb20@gmail.com>
2022-10-18 21:22:10 -0300

    atualiza

--- pai.py
+++ pai.py
@@ -6,8 +6,8 @@ class Cadastro(Tk):
         super().__init__()
 
 
-        self.frame_foto = Frame(self,bg='red',bd=1,relief='groove',width=100,height=160,)
-        self.frame_foto.pack(fill='x')
+        self.frame_foto = Frame(self,bg='red',bd=1,relief='groove')
+        self.frame_foto.place(relx=0.00,rely=0.01,relwidth=0.99,relheight=0.20)
 
         self.texto = Label(self.frame_foto,text='cadastro de pessoas ',font=('arial 12 bold'),bg='blue',fg='white')
         self.texto.pack(fill='x')
