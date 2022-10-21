d04d263254c3e1cce8ae85668fc9ede4ef99ca22
carlos alberto <carlosalbertosnb20@gmail.com>
2022-10-20 22:03:16 -0300

    atualizar

--- pai.py
+++ pai.py
@@ -1,4 +1,3 @@
-from ctypes import resize
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from PIL import Image,ImageTk
@@ -9,20 +8,24 @@ class Cadastro(Tk):
     def __init__(self ) -> None:
         super().__init__()
 
-        
-
-
         self.frame_foto = Frame(self,bg='red',width=600,height=190)
         self.frame_foto.pack()
 
         self.texto = Label(self.frame_foto,text='cadastro de pessoas ',font=('arial 15 bold'),bg='red',fg='white')
         self.texto.place(relx=0.30,rely=0.01)
 
-        self.label_foto = Label(self.frame_foto)
+        self.origem = Image.open('usuario10.png')
+        self.origem = self.origem.resize((150,150))
+        self.origem = ImageTk.PhotoImage(self.origem)
+
+        self.label_foto = Label(self.frame_foto,image=self.origem)
         self.label_foto.place(x=5,y=30,relwidth=0.25,relheight=0.80)
 
         self.bt_img = Button(self.frame_foto,text='imagem',width=10,command=self.dados_imag)
         self.bt_img.place(x=170,y=151)
+
+        self.bt_DELETA = Button(self.frame_foto,text='Excluir',width=10,command=self.deleta_img)
+        self.bt_DELETA.place(x=260,y=151)
 
     def dados_imag(self):
         global foto,img
@@ -33,11 +36,16 @@ class Cadastro(Tk):
             foto = ler_img.read()
             ler_img.close()
 
-            
-            img = ImageTk.PhotoImage(Image.open(path))
+            img = Image.open(path)
+            img = img.resize((150,150))
+            img = ImageTk.PhotoImage(img)
             self.label_foto['image'] = img
 
-            
+    def deleta_img(self):
+        pass
+
+
+
 
 if __name__=='__main__':
     ja = Cadastro()
