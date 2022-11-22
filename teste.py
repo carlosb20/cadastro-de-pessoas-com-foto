9a50ab4a8790ffb6d262e4bfd3aa51cdb805bd7f
carlos alberto <carlosalbertosnb20@gmail.com>
2022-11-21 21:19:30 -0300

    atualizacao

--- teste.py
+++ teste.py
@@ -12,24 +12,36 @@ class Teste(Tk):
         self.entrda.bind('<KeyRelease>',self.da)
         self.entrda.place(x=100,y=100)
 
+        self.cpf = Entry(self,font='arial 15')
+        self.cpf.place(x=100,y=130)
+
+
+        self.bt = Button(self,text='enviar',command=self.ver)
+        self.bt.place(x=150,y=180)
+
     def da(self,s):
-        print('ok')
+
         res = self.entrda.get()
         re = len(res)
+
         if re == 3:
-            print('passou')
             self.pega.set(str(res) +'.')
             pyautogui.press(['right'])
            
         if re == 7:
-            print('passou')
             self.pega.set(str(res) +'.')
             pyautogui.press(['right'])
            
         if re == 11:
-            print('passou')
             self.pega.set(str(res) +'-')
             pyautogui.press(['right'])
+
+        elif re > 14:
+            self.entrda.delete(14,'end')
+            self.cpf.focus()
+
+    def ver(self):
+        print(self.entrda.get())
            
 
 if __name__=='__main__':
