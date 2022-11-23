7fd05c83e5f7f314f93d5e002d5042ecf2b5b645
carlos alberto <carlosalbertosnb20@gmail.com>
2022-11-22 23:25:52 -0300

    atualizado

--- teste.py
+++ teste.py
@@ -8,16 +8,24 @@ class Teste(Tk):
 
         self.pega = StringVar()
 
+        self.traco = StringVar()
+
         self.entrda = Entry(self,font='arial 15',textvariable=self.pega)
         self.entrda.bind('<KeyRelease>',self.da)
         self.entrda.place(x=100,y=100)
 
-        self.cpf = Entry(self,font='arial 15')
+        self.cpf = Entry(self,font='arial 15',textvariable=self.traco)
         self.cpf.place(x=100,y=130)
 
 
         self.bt = Button(self,text='enviar',command=self.ver)
         self.bt.place(x=150,y=180)
+
+        self.fromata = Button(self,text='formata',command=self.forme)
+        self.fromata.place(x=200,y=180)
+
+        self.d = Button(self,text='ponto e traco',command=self.coloca_ponto)
+        self.d.place(x=300,y=180)
 
     def da(self,s):
 
@@ -42,7 +50,17 @@ class Teste(Tk):
 
     def ver(self):
         print(self.entrda.get())
-           
+
+    def forme(self):
+        
+        self.novo = self.entrda.get()
+
+        self.novo = self.novo.replace('.','').replace('-','')
+
+        print(self.novo)
+
+    def coloca_ponto(self):
+        pass
 
 if __name__=='__main__':
     res = Teste()
